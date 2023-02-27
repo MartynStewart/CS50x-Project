842431a0e49ee3a8382e8b03ab90903d4ff464a0
Martyn <Martyn_Stewart89@hotmail.com>
2023-02-27 10:39:43 +0000

    bot response created

--- bot.py
+++ bot.py
@@ -1,13 +1,40 @@
+# bot.py
 import os
+import random
+
 import discord
 from dotenv import load_dotenv
 
+# Used to get loading information - Create a .env file with your own attributes
 load_dotenv()
-TOKEN = os.getenv('')
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+OWNER = os.getenv('DISCORDBOT_OWNER')
+
 client = discord.Client()
 
+# Runs when bot is started
 @client.event
 async def on_ready():
-    print(f"Bot online! {client.user}")
+    print(f"My Owner is: {OWNER}")
+    print("BOT ONLINE")
+
+
+# Runs when a message is detected
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        # IGNORE SELF MESSAGES
+        return
+
+    print(f"New message detected from {message.author}")
+    print(f"They have posted: {message.content}")
+    
+    if message.content == "!Hello":
+        if message.author == OWNER:
+            await message.reply("Hello Father")
+        else:
+            await message.reply("Hello World")
+
 
 client.run(TOKEN)
